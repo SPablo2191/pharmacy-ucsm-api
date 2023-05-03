9b0705ad2c88ce4738f49c3d19975ae168c6a873
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-02 22:25:45 -0500

    fix: setting config for database

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from database.db import initialize_db
+from config import config
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+config(app=app)
 initialize_db(app=app)
 @app.route('/')
 def index():
-    return "Hello world"
+    return "hola"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
