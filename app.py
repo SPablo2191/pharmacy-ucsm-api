1ae4aebca3af09124334cac0bf531b8410926935
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-02 23:05:20 -0500

    feat: cors added

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from database.db import initialize_db
+from flask_cors import CORS
 from config import config
 app = Flask(__name__)
 config(app=app)
 initialize_db(app=app)
+CORS(app=app)
 @app.route('/')
 def index():
     return "hola"
