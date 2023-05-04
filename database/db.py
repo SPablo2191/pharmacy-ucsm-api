59eeef8067ee137a8a6af121dc2a4552b8f1567c
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-04 14:03:14 -0500

    fix: models in database

--- database/db.py
+++ database/db.py
@@ -1,4 +1,21 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
+from .models.branch import Branch
+from .models.customer import Customer
+from .models.depot import Depot
+from .models.product import Product
+from .models.receipt import Receipt
+from .models.receiptDetail import ReceiptDetail
+from .models.stock import Stock
+
+
 db = SQLAlchemy()
 def initialize_db(app):
     db.init_app(app)
+
+def create_db(app):
+    """verificar si las tablas ya fueron creadas"""
+    if not os.environ.get('TABLES_CREATED'):
+        with app.app_context():
+            db.create_all()
+        os.environ['TABLES_CREATED'] = 'TRUE'
