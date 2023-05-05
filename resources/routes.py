7431aacaf2fa7d1500ae9054258ba5b1fb0c404f
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-05 14:07:14 -0500

    fix: update and delete stock

--- resources/routes.py
+++ resources/routes.py
@@ -3,7 +3,7 @@ from .customers import CustomersAPI,CustomerAPI
 from .products import ProductsAPI,ProductAPI
 from .branchs import BranchsAPI,BranchAPI
 from .depots import DepotsAPI
-from .stocks import StocksAPI
+from .stocks import StocksAPI,StockAPI
 url = '/api'
 def initialize_routes(api):
  api.add_resource(ReceiptsAPI, url+'/receipts')
@@ -12,6 +12,7 @@ def initialize_routes(api):
  api.add_resource(ProductsAPI,url+'/products')
  api.add_resource(ProductAPI,url+'/product/<int:id>')
  api.add_resource(StocksAPI,url+'/stocks')
+ api.add_resource(StockAPI,url+'/stock')
  api.add_resource(BranchsAPI,url+'/branchs')
  api.add_resource(BranchAPI,url+'/branch/<int:id>')
  api.add_resource(DepotsAPI,url+'/branch/<int:id>/depots')
--- resources/stocks.py
+++ resources/stocks.py
@@ -1,7 +1,8 @@
-from flask import Response, request
+from flask import Response, request,make_response
 from flask_restful import Resource
 from database.models.stock import Stock,stock_schema,stocks_schema
 from database.db import db
+from sqlalchemy import and_,or_
 
 class StocksAPI(Resource):
     def get(self):
@@ -19,25 +20,39 @@ class StocksAPI(Resource):
         db.session.commit()
         return Response(stock_schema.dumps(new_stock), mimetype="application/json", status=200)
 
-# class StockAPI(Resource):
-#     def get(self,id):
-#         existing_customer = Customer.query.get_or_404(id)
-#         return Response(customer_schema.dumps(existing_customer), mimetype="application/json", status=200)
-#     def put(self, id):
-#         existing_customer = Customer.query.get_or_404(id)
-#         body = request.get_json()
-#         data = customer_schema.load(body,session = db.session)     
-#         existing_customer.name = data.name
-#         existing_customer.lastName = data.lastName
-#         existing_customer.phoneNumber = data.phoneNumber
-#         existing_customer.address = data.address
-#         existing_customer.email = data.email
-#         existing_customer.status = data.status
-#         existing_customer.DNI = data.DNI
-#         db.session.commit()
-#         return Response(customer_schema.dumps(existing_customer), mimetype="application/json", status=200)
-#     def delete(self,id):
-#         existing_customer = Customer.query.get_or_404(id)
-#         existing_customer.status = False
-#         db.session.commit()
-#         return Response(customer_schema.dumps(existing_customer), mimetype="application/json", status=200)
+class StockAPI(Resource):
+    def get(self):
+        product_id = request.args.get('product_id')
+        depot_id = request.args.get('depot_id')
+        stock_id = request.args.get('id')
+        existing_stock =  Stock.query.filter(or_(and_(Stock.product_id==product_id,Stock.depot_id==depot_id),Stock.id == stock_id)).all()
+        print(existing_stock)
+        if existing_stock:
+            return Response(stocks_schema.dumps(existing_stock), mimetype="application/json", status=200)
+        else:
+            return make_response('stock not found', 404)
+    def put(self):
+        product_id = request.args.get('product_id')
+        depot_id = request.args.get('depot_id')
+        stock_id = request.args.get('id')
+        existing_stock =  Stock.query.filter(or_(and_(Stock.product_id==product_id,Stock.depot_id==depot_id),Stock.id == stock_id)).all()
+        if existing_stock:
+            body = request.get_json()
+            data = stock_schema.load(body,session = db.session) 
+            existing_stock[0].quantity = data.quantity
+            existing_stock[0].status = data.status
+            db.session.commit()  
+            return Response(stocks_schema.dumps(existing_stock), mimetype="application/json", status=200)
+        else:
+            return make_response('stock not found', 404)
+    def delete(self):
+        product_id = request.args.get('product_id')
+        depot_id = request.args.get('depot_id')
+        stock_id = request.args.get('id')
+        existing_stock =  Stock.query.filter(or_(and_(Stock.product_id==product_id,Stock.depot_id==depot_id),Stock.id == stock_id)).all()
+        if existing_stock:
+            existing_stock[0].status = False
+            db.session.commit()  
+            return Response(stocks_schema.dumps(existing_stock), mimetype="application/json", status=200)
+        else:
+            return make_response('stock not found', 404)
